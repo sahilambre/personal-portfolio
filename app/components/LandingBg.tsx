79e2994385c6d76c1118de0b878e4aa8dbde0986
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-07-13 01:36:00 -0400

    :adhesive_bandage: fix landing-page css

--- app/components/LandingBg.tsx
+++ app/components/LandingBg.tsx
@@ -14,21 +14,29 @@ const LandingBg = () => {
         rangeY={10}
         baseRadius={1}
         backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+        className="flex items-center justify-center px-2 md:px-10 py-4 w-full h-full"
       >
-        <div className=" absolute left-30 md:left-20">
-          <WordRotate
-            className="text-4xl font-bold text-black dark:text-white"
-            words={["Hello! 👋🏼", "Namaste! 🙏🏼"]}
-          />
-          <h2 className="text-white text-2xl md:text-6xl font-semibold text-left">
-            Sahil Mukesh Ambre
-          </h2>
-          <p className="text-white text-2xl">Software Developer</p>
-          <button className="mt-5 px-4 py-2 rounded-md border border-white bg-black text-white text-sm hover:shadow-[4px_4px_0px_0px_rgba(255,255,255)] transition duration-200 flex items-center">
-            Checkout my CV
-            <ArrowRight className="ml-2" />
-          </button>
+        <div className="text-center md:text-left">
+          <div className="flex justify-center items-center">
+            <WordRotate
+              className="text-xl md:text-3xl lg:text-4xl font-bold text-black dark:text-white"
+              words={["Hello! 👋🏼", "Namaste! 🙏🏼"]}
+            />
+          </div>
+          <div className="flex justify-center">
+            <h2 className="text-white text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold">
+              Sahil Mukesh Ambre
+            </h2>
+          </div>
+          <div className="flex justify-center pt-8">
+            <p className="text-white text-lg md:text-2xl">Software Developer</p>
+          </div>
+          <div className="flex justify-center">
+            <button className="mt-5 px-4 py-2 rounded-md border border-white bg-black text-white text-sm md:text-base hover:shadow-2xl transition duration-200 flex items-center">
+              Checkout my CV
+              <ArrowRight className="ml-2" />
+            </button>
+          </div>
         </div>
       </Vortex>
     </div>
